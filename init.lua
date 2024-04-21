4e625fe185bae43a15a7cc7afa8a59f30e959f29
j-dax <67487509+j-dax@users.noreply.github.com>
2024-04-21 02:07:13 -0700

    chore: tuning

--- init.lua
+++ init.lua
@@ -1,8 +1,5 @@
-local api = vim.api
---
 -- Set vim background transparent
 local ns = 0
-api.nvim_set_hl(ns, "EndOfBuffer", {guibg = NONE})
-api.nvim_set_hl(ns, "Normal", {guibg = NONE})
-api.nvim_set_hl(ns, "SignColumn", {guibg = NONE})
-api.nvim_set_hl(ns, "IncSearch", {guibg = NONE})
+vim.api.nvim_set_hl(ns, "EndOfBuffer", {guibg = NONE})
+vim.api.nvim_set_hl(ns, "Normal", {guibg = NONE})
+vim.api.nvim_set_hl(ns, "SignColumn", {guibg = NONE})
